dc175eb015adadfb0d6978763084a3a056b5b0c8
Barnali Majumder <130478611+barnalii@users.noreply.github.com>
2023-09-25 19:59:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Todo: Make M+ M- and MC functional
 let string = "";
-let buttons = document.querySelectorAll('.button');
+let buttons = document.querySelectorAll('.btn');
+let btn = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
   button.addEventListener('click', (e)=>{
     if(e.target.innerHTML == '='){
@@ -18,3 +19,22 @@ Array.from(buttons).forEach((button)=>{
       }
   })
 })
+Array.from(btn).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
+
+
